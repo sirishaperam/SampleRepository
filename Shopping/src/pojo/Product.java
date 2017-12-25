8e3004df75980fe0b12d5c51c543364a4970a8e5
sirishaperam <34687952+sirishaperam@users.noreply.github.com>
2017-12-25 15:53:42 +0530

    updated
    a private variable added

--- Shopping/src/pojo/Product.java
+++ Shopping/src/pojo/Product.java
@@ -10,6 +10,7 @@ public class Product
 	private int product_quantity;
 	private String product_color;
 	private Image_pojo p_img_id;
+	private String name;
 	
 	public int getProduct_id() {
 		return product_id;
